5598e28ea82474e110965c8a7598242440e53d47
Luis Juarez <luisisthejuarez@gmail.com>
2021-03-18 00:16:45 -0400

    fixed imagepath

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Intro = () => {
-    return (
-        <div className="ui container">
-            <h1>Hi, I'm Luis Juarez</h1>
-            <p>I'm a developer, musician, and creator. I work as a Senior Analyst in the world of Services.</p>
-            <div className="ui dividing header"></div>
-            <div className="ui centered card">
-                <div className="image">
-                    <img src="https://instagram.ffcm1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/22159222_1904501699802062_6833410422406643712_n.jpg?_nc_ht=instagram.ffcm1-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=AT8n1CLwRPMAX9oAVpU&oh=d8158503a691da979df51030926d3018&oe=5F9AF0EF" alt="me" />
-                </div>
-                <div className="content">
-                    <div className="header" href="/">Luis Juarez</div>
-                    <div className="meta">
-                        <span className="date">That's me</span>
-                    </div>
-                    <div className="description">
-                        
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="ui container">
+      <h1>Hi, I'm Luis Juarez</h1>
+      <p>
+        I'm a developer, musician, and creator. I work as a Senior Analyst in
+        the world of Services.
+      </p>
+      <div className="ui dividing header"></div>
+      <div className="ui centered card">
+        <div className="image">
+          <img src="www.newcode.ml/assets/045A0066.jpeg" alt="Luis Juarez" />
         </div>
-    )
-}
+        <div className="content">
+          <div className="header" href="/">
+            Luis Juarez
+          </div>
+          <div className="meta">
+            <span className="date">That's me</span>
+          </div>
+          <div className="description"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
